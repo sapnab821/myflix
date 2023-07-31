bd7e32c803a59a32d1bc37864d1cb878749623a6
Sapna <sapnabolikal@gmail.com>
2023-07-31 17:50:16 -0400

    ready for live deployment

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const app = express(),
  Movies = Models.Movie,
  Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
  //bodyParser = require('body-parser'),
  methodOverride = require('method-override');
